c52e352441d0b1f6e108e08bff28cc7ff97594d9
Dave <dave@webdave.de>
2024-07-01 21:44:37 +0200

    gemini eingebunden

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,41 @@
-import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterOutlet, AsyncPipe],
+  template: `
+    <input [value]="q" #questionInput type="text" />
+    <button (click)="prompt(questionInput)">send</button>
+    <hr />
+    @for(h of history;track $index){
+    <div>
+      <small>{{ h.q }}:</small><br />
+      @for(s of h.a;track $index){
+      <strong>{{ s }}</strong
+      ><br />
+      }
+      <hr />
+    </div>
+    }
+  `,
 })
-export class AppComponent {
-  title = 'tmp-sx';
+export class AppComponent implements OnInit {
+  chatSession: any;
+  q = '';
+  ai = (window as any)['ai'];
+  history: { q: string; a: string[] }[] = [];
+  async ngOnInit() {
+    this.chatSession = await this.ai.createTextSession();
+  }
+
+  async prompt(input: HTMLInputElement) {
+    const a = await this.chatSession.prompt(input.value);
+    const p: { q: string; a: string[] } = { q: input.value, a: a.split('\n') };
+    this.history.push(p);
+    console.log(a);
+    input.value = '';
+  }
 }
